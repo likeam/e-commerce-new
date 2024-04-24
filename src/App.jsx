3c99078bb032ef09a5d32a77c4604946f59da645
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-24 15:32:41 +0500

    Buildig Rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import LoginForm from './pages/LoginForm'
+import Privacy from './pages/Privacy'
+import Signup from './pages/Signup'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home /> } />
+        <Route path='/login' element={<LoginForm /> } />
+        <Route path='/privacy' element={<Privacy /> } />
+        <Route path='/signup' element={<Signup /> } />
+        <Route path='/contact' element={<Contact /> } />
+       </Routes>
+      <Footer />
+    </BrowserRouter>
+    </>
   )
 }
 
